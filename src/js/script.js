c332297acaf81fb292b4cbdd8fee62264d6c27de
Aizen <ae.270718@gmail.com>
2023-04-01 23:27:03 +0600

    1-april commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
-        arrows: true
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/slide-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/slide-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  arrows: false,
+                  dots: true
+                }
+              }
+        ]
     });
   });
